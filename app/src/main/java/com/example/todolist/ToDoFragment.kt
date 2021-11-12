a10f15cdeeb38707ebed313a4e31d1418ed6d155
bayanalharthy <Bayan.Mastour.jed.k2@tuwaiq.edu.sa>
2021-11-12 21:37:28 +0300

    icon

--- app/src/main/java/com/example/todolist/ToDoFragment.kt
+++ app/src/main/java/com/example/todolist/ToDoFragment.kt
@@ -1,7 +1,6 @@
 package com.example.todolist
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -22,13 +21,13 @@ class ToDoFragment : Fragment(), DatePickerDialogFragment.DatePickercallBakc {
 
     private lateinit var titleEditText: EditText
     private lateinit var toDoList: ToDoList
-    private lateinit var dateBtn: Button
+    private lateinit var dateBtn: ImageView
     private lateinit var doneCheckBox: CheckBox
     private lateinit var descriptionEditText: EditText
 
     //private lateinit var addBtn: Button
-    private lateinit var deleteBtn: Button
-    private lateinit var add1Btn: Button
+    private lateinit var deleteBtn: ImageView
+    private lateinit var add1Btn: ImageView
     private lateinit var WelcomeTxt: TextView
 
     private var toDoListData = ToDoList()
@@ -175,7 +174,7 @@ class ToDoFragment : Fragment(), DatePickerDialogFragment.DatePickercallBakc {
                 it?.let {
                     toDoList = it
                     titleEditText.setText(it.title)
-                    dateBtn.text = it.date.toString()
+                //    dateBtn.text = it.date.toString()
                     doneCheckBox.isChecked = it.done ?: false
                 }
             }
@@ -191,7 +190,7 @@ class ToDoFragment : Fragment(), DatePickerDialogFragment.DatePickercallBakc {
     override fun onDateSelected(date: Date) {
 
         toDoList.date = date
-        dateBtn.text = date.toString()
+   //     dateBtn.text = date.toString()
     }
 }
 
